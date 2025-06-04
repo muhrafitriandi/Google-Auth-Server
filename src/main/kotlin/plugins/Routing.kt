dcd90677bea9af73a9aa6859067f968b59fc0e46
Muhammad Rafi Triandi <education.triandy@gmail.com>
2025-06-05 04:14:36 +0700

    feat (app): TokenVerification - Add User to Mongo DB

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -1,16 +1,20 @@
 package com.yandey.plugins
 
+import com.yandey.domain.repository.UserDataSource
 import com.yandey.routes.authorizedRoute
 import com.yandey.routes.rootRoute
 import com.yandey.routes.tokenVerificationRoute
 import com.yandey.routes.unauthorizedRoute
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
+import org.koin.java.KoinJavaComponent.inject
 
 fun Application.configureRouting() {
     routing {
+        val userDataSource: UserDataSource by inject(UserDataSource::class.java)
+
         rootRoute()
-        tokenVerificationRoute(application)
+        tokenVerificationRoute(application, userDataSource)
         authorizedRoute()
         unauthorizedRoute()
     }
--- src/main/kotlin/routes/TokenVerificationRoute.kt
+++ src/main/kotlin/routes/TokenVerificationRoute.kt
@@ -6,7 +6,9 @@ import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.gson.GsonFactory
 import com.yandey.domain.model.ApiRequest
 import com.yandey.domain.model.Endpoint
+import com.yandey.domain.model.User
 import com.yandey.domain.model.UserSession
+import com.yandey.domain.repository.UserDataSource
 import com.yandey.util.Constants.WEB_CLIENT_ID
 import com.yandey.util.Constants.ISSUER
 import io.ktor.server.application.Application
@@ -14,22 +16,21 @@ import io.ktor.server.application.log
 import io.ktor.server.request.receive
 import io.ktor.server.response.respondRedirect
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.RoutingContext
 import io.ktor.server.routing.post
 import io.ktor.server.sessions.sessions
 import io.ktor.server.sessions.set
 
-fun Route.tokenVerificationRoute(app: Application) {
+fun Route.tokenVerificationRoute(
+    app: Application,
+    userDataSource: UserDataSource,
+) {
     post(Endpoint.TokenVerification.path) {
         val request = call.receive<ApiRequest>()
         if (request.tokenId.isNotEmpty()) {
             val result = verifyGoogleTokenId(tokenId = request.tokenId)
             if (result != null) {
-                val name = result.payload["name"].toString()
-                val email = result.payload["email"].toString()
-                app.log.info("TOKEN VERIFICATION SUCCESSFUL: $name - $email")
-
-                call.sessions.set(UserSession(id = "123", name = "Yandey"))
-                call.respondRedirect(Endpoint.Authorized.path)
+                saveUserToDatabase(app = app, result = result, userDataSource = userDataSource)
             } else {
                 app.log.info("TOKEN VERIFICATION FAILED")
                 call.respondRedirect(Endpoint.Unauthorized.path)
@@ -50,5 +51,32 @@ fun verifyGoogleTokenId(tokenId: String): GoogleIdToken? {
         verifier.verify(tokenId)
     } catch (e: Exception) {
         null
+    }
+}
+
+private suspend fun RoutingContext.saveUserToDatabase(
+    app: Application,
+    result: GoogleIdToken,
+    userDataSource: UserDataSource
+) {
+    val sub = result.payload["sub"].toString()
+    val name = result.payload["name"].toString()
+    val emailAddress = result.payload["email"].toString()
+    val profilePhoto = result.payload["picture"].toString()
+    val user = User(
+        id = sub,
+        name = name,
+        email = emailAddress,
+        profilePhoto = profilePhoto
+    )
+
+    val response = userDataSource.saveUserInfo(user = user)
+    if (response) {
+        app.log.info("USER SUCCESSFULLY SAVED/RETRIEVED")
+        call.sessions.set(UserSession(id = sub, name = name))
+        call.respondRedirect(Endpoint.Authorized.path)
+    } else {
+        app.log.info("ERROR SAVING THE USER")
+        call.respondRedirect(Endpoint.Unauthorized.path)
     }
 }
