f377d93836a598c584dcab88a5d9a849ac58e431
Muhammad Rafi Triandi <education.triandy@gmail.com>
2025-06-05 03:18:29 +0700

    feat (app): create and implement user data source

--- src/main/kotlin/di/KoinModule.kt
+++ src/main/kotlin/di/KoinModule.kt
@@ -1,5 +1,7 @@
 package com.yandey.di
 
+import com.yandey.data.repository.UserDataSourceImpl
+import com.yandey.domain.repository.UserDataSource
 import com.yandey.util.Constants.DATABASE_NAME
 import org.koin.dsl.module
 import org.litote.kmongo.coroutine.coroutine
@@ -10,5 +12,8 @@ val koinModule = module {
         KMongo.createClient()
             .coroutine
             .getDatabase(DATABASE_NAME)
+    }
+    single<UserDataSource> {
+        UserDataSourceImpl(get())
     }
 }
---
+++ src/main/kotlin/domain/model/User.kt
@@ -0,0 +1,11 @@
+package com.yandey.domain.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User(
+    val id: String,
+    val name: String,
+    val email: String,
+    val profilePhoto: String,
+)
